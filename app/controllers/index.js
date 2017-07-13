2187eb54d591c9734388f2617889d9d8b5c1db2a
axetroy <troy450409405@gmail.com>
2017-07-13 15:32:30 +0800

    init

--- app/controllers/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * Created by axetroy on 17-7-13.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphQL_online_1 = require("./graphQL-online");
-exports.graphQLonline = graphQL_online_1.default;
