42b0c3ed7477d07138b97fedffa8b4208e40cb5e
axetroy <troy450409405@gmail.com>
2017-07-17 15:14:42 +0800

    update

--- app/config.ts
+++ app/config.ts
@@ -1,19 +1,30 @@
 /**
  * Created by axetroy on 17-7-13.
  */
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+
+const PROJECT_ROOT_PATH = path.join(__dirname, '../');
+
+const PROJECT_CONFIG_PATH =
+  process.env.NODE_ENV === 'production'
+    ? process.env.PROJECT_CONFIG_PATH
+    : path.join(PROJECT_ROOT_PATH, 'config.yaml');
 
 interface Db$ {
-  host: string
+  host: string;
 }
 
 interface CONFIG$ {
-  db: Db$
+  name: string;
+  userPubPath: string;
+  adminPubPath: string;
+  db: Db$;
 }
 
-const CONFIG: CONFIG$ = {
-  db: {
-    host: 'localhost'
-  }
-};
+const CONFIG: CONFIG$ = yaml.safeLoad(
+  fs.readFileSync(PROJECT_CONFIG_PATH, 'utf8')
+);
 
 export default CONFIG;
--- app/server.ts
+++ app/server.ts
@@ -2,35 +2,52 @@
  * Created by axetroy on 17-7-13.
  */
 
-// Import everything from express and assign it to the express constiable
 import * as express from 'express';
-import {graphql} from 'graphql';
-import * as graphqlHTTP from  'express-graphql';
+import { graphql } from 'graphql';
+import * as graphqlHTTP from 'express-graphql';
+const fs = require('fs');
+const Promise = require('bluebird');
+const jwt = Promise.promisifyAll(require('jsonwebtoken'));
 
-// Import WelcomeController from controllers entry point
+import CONFIG from './config';
+
 import schema from './graphql';
 
-// Create a new express application instance
 const app: express.Application = express();
-// The port the express app will listen on
 const PORT: number = Number(process.env.PORT) || 3000;
 
-app.use('/', graphqlHTTP({
-  schema: schema,
-  // rootValue: root,
-  graphiql: true  //Set to false if you don't want graphiql enabled
-}));
+app.get('/token/:token', (req, res) => {
+  const cert = fs.readFileSync(CONFIG.userPubPath, 'utf8');
+  jwt
+    .verifyAsync(req.params.token, cert)
+    .then((decoded: any) => {
+      if (new Date(decoded.exp * 1000) > new Date()) {
+        res.status(200).json({ error: [{ message: 'Token has expired' }] });
+      } else {
+        res.status(200).json(decoded);
+      }
+    })
+    .catch((err: Error) => {
+      res.status(200).json({ error: [{ message: 'Invalid token' }] });
+    });
+});
+
+app.use(
+  '/api/v1/graphql',
+  graphqlHTTP({
+    schema: schema,
+    // rootValue: root,
+    graphiql: true //Set to false if you don't want graphiql enabled
+  })
+);
 
 app.post('/graphql', (req, res) => {
   //GraphQL executor
-  graphql(schema, req.body)
-    .then((result) => {
-      res.status(200).json(result);
-    })
+  graphql(schema, req.body).then(result => {
+    res.status(200).json(result);
+  });
 });
 
-// Serve the application at the given port
 app.listen(PORT, () => {
-  // Success callback
   console.log(`Listening at http://localhost:${PORT}/`);
 });
